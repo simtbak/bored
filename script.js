e00d1c2bfd96edff15ff3284d6d80d2da0283dbc
Simeon <simtbak@users.noreply.github.com>
2021-12-14 21:18:16 +0000

    snap pugs

---
+++ script.js
@@ -0,0 +1,65 @@
+leftposition = 0;
+topposition = 0;
+
+function setpuggle () {
+  puggle = JSON.parse(this.responseText).message;
+  environment = document.getElementById('environment')
+  environment.setAttribute("src",puggle);
+}
+
+getpuggle = function() {
+	var oReq = new XMLHttpRequest();
+	oReq.addEventListener("load", setpuggle);
+	oReq.open("GET", "https://dog.ceo/api/breed/pug/images/random");
+	oReq.send();
+}
+
+
+character = function() { 
+	const newDiv = document.createElement("div");
+	newDiv.className = 'protaganist';
+	newDiv.id = 'bob';
+	document.body.appendChild(newDiv);
+}
+
+movement = function(direction,character) {
+	speed = 124;
+	if(direction == 'L') { 
+		leftposition += 0-speed
+	}
+	if(direction == 'R') { 
+		leftposition += speed
+	}
+	if(direction == 'U') { 
+		topposition += 0-speed
+	}
+	if(direction == 'D') { 
+		topposition += speed
+	}
+	character.style.left = leftposition + "px";
+	character.style.top = topposition + "px";
+}
+
+snap = function(type,character) {
+	console.log(type);
+	if(type == 'Space') {
+		getpuggle();
+		character.style.boxShadow = "0 0 124px yellow";
+		setTimeout(function() {character.style.boxShadow = "0 0 0px blue"},100);
+	}
+		
+}
+
+function logkey(e) {
+  c = document.getElementById('bob');
+  if(e.code.substr(0,5) == 'Arrow') {
+	movement(e.code.substr(5,1),c);
+  } 
+  else {
+	snap(e.code,c);
+  }
+}
+
+character();
+document.addEventListener('keydown', logkey);
+getpuggle();
